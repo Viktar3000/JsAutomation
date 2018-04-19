601974ecdae4a0d9c6cfa9239e3830c07bbf0213
Viktar_Ivanou <viktar_ivanou@epam.com>
2018-04-19 14:24:51 +0300

    code style improvements for pages

--- src/po/MainPage.js
+++ src/po/MainPage.js
@@ -2,21 +2,18 @@ const AbstractPage = require("./AbstractPage");
 const EC = protractor.ExpectedConditions;
 const expect = require("chai").expect;
 
-
-
 class MainPage extends AbstractPage {
-        constructor(customBrowser){
-        super(customBrowser);
 
+        constructor(customBrowser) {
+        super(customBrowser);
         this.composeButton = this.browser.element(by.css('div.z0 div.L3'));
         this.toField = this.browser.element(by.css('textarea[name=to]'));
         this.messageField = this.browser.element(by.css('div.Ar.Au div.Am'));
         this.sendMessageButton = this.browser.element(by.css('div.aoO'));
-        this
+        
         }
 
-         async sendMessage(to, messageBody){
-
+         async sendMessage(to, messageBody) {
 
               await this.composeButton.click();
               browser.wait(EC.visibilityOf(this.toField), 10000);
@@ -24,14 +21,16 @@ class MainPage extends AbstractPage {
               browser.wait(EC.visibilityOf(this.messageField), 3000);
               await this.messageField.sendKeys(messageBody);
               await this.sendMessageButton.click();
+
         }
 
         async checkEmail() {
+
                 element(by.css("span.y2")).isDisplayed().then(isDisplayed => {
                         expect(isDisplayed).to.equal(true);
+
                 });
         }
 
 }
-
 module.exports = MainPage;
